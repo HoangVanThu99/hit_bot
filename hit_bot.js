1268b756c11cde122f3ba55ba579d3d1a452b6b7
ThunderSword99 <guardian.of.gods99@gmail.com>
2021-06-01 15:35:34 +0700

    Initial commit

---
+++ hit_bot.js
@@ -0,0 +1,250 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json')
+const schedule = require('node-schedule')
+const moment = require ("moment-timezone")
+const services = require('./services.js')
+
+moment.tz.setDefault("Asia/Ho_Chi_Minh");
+
+client.on('ready',()=>{
+    client.user.setActivity(' with hiter',{type:'PLAYING'})
+    
+    let server = client.guilds.cache.get(config.HIT_GUILD_ID);
+    let channel = client.channels.cache.get("843116118448406538")
+    //console.log(server.members.cache.filter(member => member.presence.status !== "offline").map(user => user.id))
+
+    const sleepingTime= new schedule.RecurrenceRule();
+    sleepingTime.hour = [0];
+    sleepingTime.minute = 0;
+    sleepingTime.second = 0;
+
+    schedule.scheduleJob(
+      sleepingTime,
+      async () => {
+        onlineMemberList = await server.members.cache.filter(member => member.presence.status !== "offline").map(user => user.id)
+        var content = "Bây giờ là 24 giờ. Muộn rồi đi ngủ sớm thôi các bạn ơi"
+        var strTag = ``
+        onlineMemberList.forEach(memberID =>{
+            strTag += `<@${memberID}>`
+        })
+        channel.send(content + "\n" + strTag)
+      })
+      
+    const weatherTime = new schedule.RecurrenceRule();
+    weatherTime.hour = [7];
+    weatherTime.minute = 0;
+    weatherTime.second = 0;
+  
+    schedule.scheduleJob(
+      weatherTime,
+      async () => {
+        channel.send(`Bây giờ là: ${moment()
+          .locale("vi").format("HH:mm:ss")}`);
+  
+        const weatherData = await services.getWeatherData()
+        channel.send(services.getWeatherEmbeded(weatherData))
+      });
+
+
+    const covidTime = new schedule.RecurrenceRule();
+    covidTime.hour = [6,12,18];
+    covidTime.minute = 0;
+    covidTime.second = 0;
+  
+    schedule.scheduleJob(
+       //"* * *6 * * *",
+      covidTime,
+      async () => {
+        channel.send(`Bây giờ là: ${moment()
+          .locale("vi").format("HH:mm:ss")}`);
+  
+        const covidData = await services.getCovidData();
+        channel.send(services.getCovidEmbeded(covidData));
+      });
+})
+
+client.on('guildMemberAdd', member => {
+  const str = `Chào mừng  <@${member.user.id}> đến với cộng đồng **HIT COMUNITY**. Đây là cộng đồng CLB HIT trên nền tảng Discord.`
+  + `\nTôi là hit bot, nếu cần được trợ giúp hãy sử dụng câu lệnh ***hit:help***. Tôi luôn có mặt ở mọi nơi, mọi chỗ ^^`
+  member.guild.channels.cache.get('820253588769079316')
+  .send(str).then(msg => msg.delete({timeout:60000}))
+})
+
+//Lắng nghe sự kiện khi có người nhập nội dung
+client.on('message', (message) =>{
+    try {
+      if (message.author == client.user || message.member.roles.cache.some((role) => role.name == "bot"))
+      {
+          return;
+      }
+    } catch(err) {
+        return
+    }
+    
+
+    if (message.content.toLowerCase()=="bot ngu")
+    {
+       const attachment = new Discord.MessageAttachment('Image/Meme/nguoianhemodau.jpg')
+       
+       message.channel.send(attachment)
+    }
+
+    if (message.content.substr(0,4).toLowerCase()==='hit:') {
+        services.processCommand(message)
+    }
+})
+
+const events = {
+	MESSAGE_REACTION_ADD: 'messageReactionAdd',
+	MESSAGE_REACTION_REMOVE: 'messageReactionRemove',
+};
+
+client.on('raw', async event => {
+	if (!events.hasOwnProperty(event.t)) return;
+	const { d: data } = event;
+	const user = client.users.cache.get(data.user_id);
+	const channel = client.channels.cache.get(data.channel_id) || await user.createDM();
+	if (channel.messages.cache.has(data.message_id)) return;
+	const message = await channel.messages.fetch(data.message_id);
+	const emojiKey = (data.emoji.id) ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name;
+	const reaction = message.reactions.cache.get(emojiKey); 
+	client.emit(events[event.t], reaction, user);
+});
+
+client.on('messageReactionAdd', (reaction, user) => {
+  try {
+    if (reaction.message.guild.id != config.HIT_GUILD_ID || reaction.message.channel.id != "820903432361476106")
+    {
+      return;
+    }
+  } catch(err) {
+      return
+  }
+  
+  switch (reaction.emoji.name)
+  {
+    case "mobile":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('821061956672815156');
+      member.roles.add(role);
+      break
+    case "photoshop":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('821022281106980884');
+      member.roles.add(role);
+      break
+    case "unity":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('820826091375558677');
+      member.roles.add(role);
+      break
+    case "algorithm":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('821773418164125757');
+      member.roles.add(role);
+      break
+    case "web":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('821205588818133012');
+      member.roles.add(role);
+      break
+    case "mercylearning":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('821053949804544000');
+      member.roles.add(role);
+      break
+
+    case "🎮":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('820903027430653963');
+      member.roles.add(role);
+      break
+    case "🎥":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('822791975572602942');
+      member.roles.add(role);
+      break
+    case "anime":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('820901954590343198');
+      member.roles.add(role);
+      break
+    case "⛹️":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('823127789599195136');
+      member.roles.add(role);
+      break
+  }
+  
+  
+});
+
+client.on('messageReactionRemove', (reaction, user) => {
+  try {
+    if (reaction.message.guild.id != config.HIT_GUILD_ID || reaction.message.channel.id != "820903432361476106")
+    {
+      return;
+    }
+  } catch(err) {
+      return
+  }
+  
+  switch (reaction.emoji.name)
+  {
+    case "mobile":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('821061956672815156');
+      member.roles.remove(role);
+      break
+    case "photoshop":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('821022281106980884');
+      member.roles.remove(role);
+      break
+    case "unity":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('820826091375558677');
+      member.roles.remove(role);
+      break
+    case "algorithm":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('821773418164125757');
+      member.roles.remove(role);
+      break
+    case "web":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('821205588818133012');
+      member.roles.remove(role);
+      break
+    case "mercylearning":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('821053949804544000');
+      member.roles.remove(role);
+      break
+
+    case "🎮":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('820903027430653963');
+      member.roles.remove(role);
+      break
+    case "🎥":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('822791975572602942');
+      member.roles.remove(role);
+      break
+    case "anime":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('820901954590343198');
+      member.roles.remove(role);
+      break
+    case "⛹️":
+      var member = reaction.message.guild.members.cache.get(user.id);
+      var role = reaction.message.guild.roles.cache.get('823127789599195136');
+      member.roles.remove(role);
+      break
+  }
+});
+
+// Login: Điền token (mật mã để truy cập)
+client.login(config.BOT_TOKEN)
---
+++ services.js
@@ -0,0 +1,355 @@
+const axios =  require('axios')
+const moment = require ('moment-timezone')
+const config = require('./config.json')
+const excelToJson = require('convert-excel-to-json');
+
+const Discord = require('discord.js')
+const fs =  require("fs");
+
+const processCommand = (message) => {
+    
+    let fullCommand = message.content.substr(4,message.content.length)
+    if (fullCommand==null || fullCommand =='') return;
+    let splitCommand = fullCommand.split(" ")
+    let cmd = splitCommand[0].toLowerCase()
+    let arguments = splitCommand.slice(1)
+    let firstArg = splitCommand[1]
+    let secondArg = splitCommand[2]
+    let thirdArg = splitCommand[3]
+
+    // console.log(splitCommand)
+    // console.log("Command received: " + cmd)
+    // console.log("Arguments: " + arguments)
+    // console.log("Arguments 1: " + firstArg)
+    // console.log("Arguments 2: " + secondArg)
+    // console.log("Arguments 3: " + thirdArg)
+
+    switch(cmd)
+    {
+        case "commands":
+            Commands(message)
+            break
+        case "joke":
+            Joke(message,arguments)
+            break
+        case "anime":
+            ShowAnimeImage(message)
+            break
+        case "get_user_react":
+            GetUserReact(message,arguments)
+            break
+        case "ask":
+            AskQuestion(message,arguments)
+            break
+        case "covid":
+            Covid(message);
+            break
+        case "meme":
+            Meme(message,arguments)
+            break
+        case "girl":
+            Girl(message)
+            break
+        case "boy":
+            Boy(message)
+            break
+        case "poetry":
+            Poetry(message)
+            break
+        case "weather":
+            Weather(message)
+            break
+        case "level":
+            break
+        case "help":
+            break
+        case "setlevel":
+            break
+        case "leaderboard":
+            break
+        case "test":
+            Test(message)
+            break
+        default:
+            message.channel.send("Bot không hỉu, bot không hỉu, bot không hỉu")
+            break;
+    }
+}
+
+const Test = (message) =>{
+    onlineMemberList = message.guild.members.cache.filter(member => member.presence.status !== "offline").map(user => user.id)
+    console.log(onlineMemberList)
+    var strTag = ``
+    var content = "Bây giờ là 24 giờ. Muộn rồi đi ngủ sớm thôi các bạn ơi"
+    onlineMemberList.forEach(memberID =>{
+        strTag += `<@${memberID}>`
+    })
+    message.channel.send(content + "\n" + strTag)
+}
+
+const Commands = (message) =>{
+    const result = excelToJson({
+        sourceFile: 'Data/bot_commands.xlsx',
+        columnToKey: {
+            A: '{{A1}}',
+            B: '{{B1}}',
+            C: '{{C1}}',
+            D: '{{D1}}',
+            E: '{{E1}}',
+        }
+    });
+    const commands = result.Sheet1.map(res =>{
+        const name = res.cau_lenh;
+        const value =  res.mieu_ta;
+        return {name,value,inline:false};
+    }).splice(1);
+    var commandsList = new Discord.MessageEmbed()
+    .setColor("#0099ff")
+    .setTitle("Danh sách câu lệnh")
+    .addFields(commands)
+    message.channel.send(commandsList)
+
+}
+
+const animeImages = fs.readdirSync(config.ANIME_IMAGE_FOLDER);
+const ShowAnimeImage = (receivedMessage) => {
+     // Lấy tất cả ảnh thành một list trong folder Image/Anime
+    // Lấy random ảnh anime
+    const attachment = new Discord.MessageAttachment(config.ANIME_IMAGE_FOLDER + animeImages[Math.floor(Math.random() * animeImages.length)])
+    receivedMessage.channel.send(attachment)
+};
+
+const Joke = (message, arguments) =>
+{
+    joke = arguments[0];
+    switch (joke)
+    {
+        case 'ohno':
+            setTimeout(() => {  message.channel.send("Âu nâu"); }, 1000);
+            setTimeout(() => {  message.channel.send("Âu nâu"); }, 3000);
+            setTimeout(() => {  message.channel.send("Âu nấu nâu nâu nâu nâu"); }, 5000);
+            break
+        default:
+            message.channel.send("Bạn đã dùng sai cú pháp. Bạn là chúa hề!!")
+            break
+    }
+}
+
+const GetUserReact = (message,arguments) =>
+{
+    message.guild.channels.cache.forEach((channel)=>{
+        try
+        {
+            channel.messages.fetch(arguments[0]).then((message) =>{
+                message.reactions.cache.forEach(reaction=>{
+                    reaction.users.fetch().then(user => user.map((user) => message.channel.send(user.username)))
+                })
+            })
+        }
+        catch(err)
+        {
+            
+        }
+    })
+}
+
+
+const Meme = (message,arguments) =>
+{
+    //console.log(arguments[0])
+    meme = arguments[0]
+    if (meme === undefined)
+    {
+        const memeImages = fs.readdirSync(config.MEME_IMAGE_FOLDER);
+        const attachment = new Discord.MessageAttachment(config.MEME_IMAGE_FOLDER + memeImages[Math.floor(Math.random() * memeImages.length)])
+        message.channel.send(attachment)
+        return
+    }
+    switch(meme)
+    {
+        case 'thamlam':
+            attachment = new Discord.MessageAttachment("Image/Meme/thamlam.jpeg")
+            message.channel.send(attachment)
+            break
+        case 'ngudot':
+            attachment = new Discord.MessageAttachment("Image/Meme/ngudot.jpeg")
+            message.channel.send(attachment)
+            break
+        case 'concainit':
+            attachment = new Discord.MessageAttachment("Image/Meme/concainit.jpeg")
+            message.channel.send(attachment)
+            break
+        case 'simpson':
+            attachment = new Discord.MessageAttachment("Image/Meme/simpson.png")
+            message.channel.send(attachment)
+            break
+    }
+}
+
+const Girl = (message) =>
+{
+    const girlImages = fs.readdirSync(config.HITGIRL_IMAGE_FOLDER);
+    const attachment = new Discord.MessageAttachment(config.HITGIRL_IMAGE_FOLDER + girlImages[Math.floor(Math.random() * girlImages.length)])
+    message.channel.send(attachment)
+}
+
+const Boy = (message) =>
+{
+    const boyImages = fs.readdirSync(config.HITBOY_IMAGE_FOLDER);
+    const attachment = new Discord.MessageAttachment(config.HITBOY_IMAGE_FOLDER + boyImages[Math.floor(Math.random() * boyImages.length)])
+    message.channel.send(attachment)
+}
+
+const Poetry = (message) =>
+{
+    const result = excelToJson({
+        sourceFile: config.POETRY_FILE,
+        columnToKey: {
+            A: '{{A1}}',
+            B: '{{B1}}',
+            C: '{{C1}}',
+            D: '{{D1}}',
+            E: '{{E1}}',
+        }
+    });
+    let i =0
+    result['Sheet1'][Math.floor(Math.random() * result['Sheet1'].length)]['noi_dung'].split('\n').forEach(str =>{
+        i++
+        setTimeout(() => { message.channel.send(str)}, 2000*i);
+    })
+}
+
+const AskQuestion = (message,arguments) =>
+{
+    var question = arguments.join(' ')
+    message.channel.send(question).then((message) =>{
+        message.react('✅')
+        message.react('❌')
+    })
+}
+
+
+const getCovidEmbeded = (data) => new Discord.MessageEmbed()
+  .setColor("#0099ff")
+  .setTitle("Covid 19")
+  .setURL("https://ncov.moh.gov.vn/")
+  .addFields(data)
+  .setImage("https://disease.sh/assets/img/flags/vn.png");
+
+const Covid = async (receivedMessage) => {
+  const data = await getCovidData();
+
+  receivedMessage.reply("");
+  receivedMessage.channel.send(getCovidEmbeded(data));
+}
+
+const getWeatherEmbeded = (data) => new Discord.MessageEmbed()
+    .setColor("#0099ff")
+    .setTitle("Thời tiết")
+    //.setURL("https://ncov.moh.gov.vn/")
+    .addFields(data)
+    .setImage("https://x2tienganh.com/wp-content/uploads/2019/10/have-a-nice-day.jpg");
+
+const Weather = async (receiveMessage) => {
+    const data = await getWeatherData()
+    receiveMessage.reply("")
+    receiveMessage.channel.send(getWeatherEmbeded(data))
+};
+
+const covidUrl = "https://corona.lmao.ninja/v2/countries/vn";
+const fetchApi = () => axios.get(covidUrl);
+
+const getCovidData = async () => {
+    const {data} = await fetchApi();
+    const {
+      updated,
+      country,
+      cases,
+      todayCases,
+      deaths,
+      todayDeaths,
+      recovered,
+      todayRecovered,
+      population,
+      continent,
+      undefined: undefine,
+      // active,
+      // critical,
+      // casesPerOneMillion,
+      // deathsPerOneMillion,
+      // tests,
+      // testsPerOneMillion,
+      // oneCasePerPeople,
+      // oneDeathPerPeople,
+      // oneTestPerPeople,
+      // activePerOneMillion,
+      // recoveredPerOneMillion,
+      // criticalPerOneMillion,
+    } = data;
+  
+    const arr = [
+      `Quốc gia:${country}`,
+      `Khu vực:${continent}`,
+      `Dân số:${population}`,
+      `Số ca nhiễm:${cases}`,
+      `Số ca tử vong:${deaths}`,
+      `Số ca đã phục hồi:${recovered}`,
+      `Số ca nhiễm (nay):${todayCases}`,
+      `Số ca tử vong (nay):${todayDeaths}`,
+      `Số ca đã phục hồi (nay):${todayRecovered}`,
+      `Số ca chưa xác định:${undefine}`,
+      `Cập nhật lúc:${moment(updated)
+        .locale("vi")
+        .format("HH:mm:ss, DD/MM/YYYY")}`,
+    ];
+  
+    return arr.map(e => {
+      const items = e.split(":");
+      const name = items[0];
+      const value = items.slice(1).join(":");
+      return {name, value, inline: true};
+    },
+    );
+};
+
+const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=hanoi&appid=c2364c9a824b35ae8a0c3639abc0589a"
+const fetchApiWeather = () => axios.get(weatherUrl)
+
+const getWeatherData = async () => {
+    const {data} = await fetchApiWeather();
+    const {
+      weather,
+      main,
+      clouds,
+      sys
+    } = data;
+ 
+    const arr =[
+        `Quốc gia: Việt Nam`,
+        `Thành phố: Hà Nội`,
+        `Thời tiết: ${weather[0]?.description}`,
+        `Nhiệt độ: ${main.temp - 273.15}℃`,
+        `Nhiệt độ thực: ${main.feels_like - 273.15}℃`,
+        `Độ ẩm: ${main.humidity}%`,
+        `Độ che phủ của mây: ${clouds.all}%`,
+        `Thời gian mặt trời mọc: ${moment.unix(sys.sunrise)
+            .locale("vi")
+            .format("HH:mm:ss, DD/MM/YYYY")}`,
+        `Thời gian mặt trời lặn: ${moment.unix(sys.sunset)
+            .locale("vi")
+            .format("HH:mm:ss, DD/MM/YYYY")}`
+    ]
+ 
+    return arr.map(e => {
+        const items = e.split(":");
+        const name = items[0];
+        const value = items.slice(1).join(":");
+        return {name, value, inline: true};
+    },
+    );
+};
+
+// reactionRole MessageID, EmojiID, RoleID
+
+
+module.exports = {getCovidData,getCovidEmbeded,processCommand,getWeatherData,getWeatherEmbeded}
